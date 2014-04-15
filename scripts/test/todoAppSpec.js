b6f82434313af6e4057dabfa3ef247a69c25a871
Sami Niiranen <saminiir@gmail.com>
2014-04-15 21:30:42 +0300

    Add first stub UI test

--- scripts/test/todoAppSpec.js
+++ scripts/test/todoAppSpec.js
@@ -1,5 +1,11 @@
 define(['scripts/app/todoApp'], function(TodoApp){
-    jasmine.getFixtures().fixturesPath = '.';
+    jasmine.getFixtures().fixturesPath = '.'
+
+    describe("My Feature", function() {
+      it("should add numbers", function() {
+        expect(1+1).toBe(2)
+       })
+    })
 
     describe("TodoApp", function() {
         beforeEach(function() {
@@ -8,6 +14,21 @@ define(['scripts/app/todoApp'], function(TodoApp){
 
         it("should be able to add Todo item", function() {
             var todoInput = $("#new-todo")
-        });
-    });
-});
+
+            inputValueAndPressEnter($("#new-todo"), "test")
+
+            expect(todoInput.val()).toBe("")
+        })
+    })
+
+    function inputValueAndPressEnter(elem, value) {
+            var keypress = $.Event('keypress')
+            keypress.which = 13 //ENTER keycode
+
+            var element = $(elem)
+ 
+            element.val(value)
+ 
+            element.trigger(keypress)
+    }
+})
